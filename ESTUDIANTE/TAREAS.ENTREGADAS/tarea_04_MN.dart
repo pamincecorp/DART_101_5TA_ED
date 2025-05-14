1c4f103df2352569ed80f81f2eba1043fd250ed1
Milton Nasimba <yashablank577@gmail.com>
2025-05-14 16:13:18 -0500

    upadate

---
+++ ESTUDIANTE/TAREAS.ENTREGADAS/tarea_04_MN.dart
@@ -0,0 +1,26 @@
+/* 
+Tarea No. 4
+Cuerso: App en flutter
+Nombre: Milton F. Nasimba T.
+Fecha: 14/05/2025
+*/
+
+void main() {
+  int edad = 10;
+
+  if (edad > 0 && edad <= 4) {
+    print("Bebé");
+  }
+
+  if (edad > 4 && edad < 14) {
+    print("Niño");
+  }
+
+  if (edad >= 14 && edad < 18) {
+    print("Adolescente");
+  }
+
+  if (edad >= 18) {
+    print("Mayor de edad");
+  }
+}
